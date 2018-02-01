8c10189b2cc6e8107504b716c57af81d754cd10b
Endre Sebestyen <endre.sebestyen@gmail.com>
2018-02-01 12:17:41 +0100

    Reorganizing and cleaning up datasets. - Updated DESCRIPTION file - Added LICENSE.md and README.md - Added skeleton for dataset descriptions. - Removed misc R scripts. - Moved data-raw to inst/extdata and compressed large tsv files. - Combined all metagene profiles into a single list of data frames.

---
+++ R/chic.data.R
@@ -0,0 +1,78 @@
+#' Metagene profile data.
+#'
+#' Metagene profile data for the CHIC package.
+#'
+#' @format A list of data frames with variables:
+#'
+#' @source
+#'
+#' @examples
+#'
+#'
+"compendium_profiles"
+
+#' RefSeq gene annotation.
+#'
+#' RefSeq gene annotation data for the CHIC package.
+#'
+#' @format A data frame with variables:
+#'
+#' @source
+#'
+#' @examples
+#'
+#'
+"refseq_genes_annotated"
+
+#' Filtered RefSeq gene annotation as GRanges.
+#'
+#' RefSeq gene annotation data for the CHIC package in GRanges format, filtered
+#' by ...
+#'
+#' @format A GRanges object with:
+#'
+#' @source
+#'
+#' @examples
+#'
+#'
+"refseq_genes_filtered_granges"
+
+#' RefSeq gene annotation as GRanges.
+#'
+#' RefSeq gene annotation data for the CHIC package in GRanges format.
+#'
+#' @format A GRanges object with:
+#'
+#' @source
+#'
+#' @examples
+#'
+#'
+"refseq_genes_granges"
+
+#' RefSeq gene transcription start site regions as GRanges.
+#'
+#' RefSeq gene transcription start site regions for the CHIC package in GRanges
+#' format.
+#'
+#' @format A GRanges object with:
+#'
+#' @source
+#'
+#' @examples
+#'
+#'
+"refseq_genes_tssregion_granges"
+
+
+
+
+
+
+
+
+
+
+
+
